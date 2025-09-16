d16f2a5c2f689985bb757bc58f2de610ca603d70
Almir Santos <asantosso@outlook.com>
2025-09-16 20:57:25 -0300

    Adicionando artefatos

---
+++ src/main/java/br/com/projeto/sistema_vendas/SistemaDeVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.sistema_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/sistema_vendas/SistemaDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.sistema_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
